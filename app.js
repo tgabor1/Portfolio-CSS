86131c9b0b2c9ae07bf5d5b81ae3137059fdff17
tgabor1 <t.gabor@tutanota.com>
2022-05-05 12:16:14 +0200

    Modifs, début du JS

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// Bouton du menu
+
 let button = document.getElementById("button");
 
-button.addEventListener ("click", alert("Bonjour"));
+button.addEventListener("click", affichageMenu);
+
+function affichageMenu() {
+
+}
+
+
+
+
+// Light mode 
+
+let light = document.getElementById("light");
+
+light.addEventListener("click", activerLightMode);
+
+function activerLightMode() {
+
+}
