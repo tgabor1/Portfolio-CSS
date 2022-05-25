d635ffe16287e443fc056d8199bbd5e311eea4f4
tgabor1 <t.gabor@tutanota.com>
2022-05-25 21:30:53 +0200

    modifs sur code

---
+++ app.js
@@ -0,0 +1,7 @@
+// Je déclare des variables
+const butt2 = document.getElementById('button').onclick = changerBackground
+
+// Cette fonction consiste à changer la couleur du background
+function changerBackground() {
+    header.style.backgroundColor = 'blue'
+}
